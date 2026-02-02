cbf52174f12d46bcaf9664deba69c0a831210126
tinywaves <dhzhme@gmail.com>
2026-02-03 01:34:32 +0800

    feat: add two testing files

---
+++ testing/repo_root_path.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+REPO_ROOT_PATH="$(cd "$(dirname "$0")" && pwd)"
+# echo "$REPO_ROOT_PATH"
+source "$REPO_ROOT_PATH/sub_script.sh"
---
+++ testing/sub_script.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+echo "$REPO_ROOT_PATH"
